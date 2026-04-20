23e5b8eb1c2a3a78cd8b2f147b3d33bdf56c1a32
wladimiredgar <wladimiredgar@gmail.com>
2026-04-20 16:23:49 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function getRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generarCarta() {
+  let palos = ["♥", "♠", "♦", "♣"];
+  let valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  let paloRandom = getRandom(palos);
+  let valorRandom = getRandom(valores);
+
+  let top = document.querySelector(".top");
+  let number = document.querySelector(".number");
+  let bottom = document.querySelector(".bottom");
+  let card = document.querySelector(".card");
+
+  top.innerHTML = paloRandom;
+  number.innerHTML = valorRandom;
+  bottom.innerHTML = paloRandom;
+
+  card.classList.remove("red", "black");
+
+  if (paloRandom === "♥" || paloRandom === "♦") {
+    card.classList.add("red");
+  } else {
+    card.classList.add("black");
+  }
+}
+
+window.onload = function () {
+  generarCarta();
+
+  let button = document.querySelector("#btn");
+  button.addEventListener("click", generarCarta);
+
+  setInterval(generarCarta, 10000);
 };
+
